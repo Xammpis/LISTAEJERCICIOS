9b95dda2934946d366c2402eb519964989a9c40f
= <sbouzoubaa@uoc.edu>
2021-04-10 12:04:02 +0200

    master

---
+++ src/app/collection-ejercicios.ts
@@ -0,0 +1,10 @@
+import {Ejercicio} from './ejercicio';
+
+export const EJERCICIOS: Ejercicio[]=[
+  {id: 1, name: 'Flexión pecho'},
+  {id: 2, name: 'Sentadilla'},
+  {id: 3, name: 'Curl biceps'},
+  {id: 4, name: 'Dominada'},
+  {id: 5, name: 'Planche'},
+  {id: 6, name: 'Press mancuerna hombro'}
+];
--- src/app/ejercicios/ejercicios.component.ts
+++ src/app/ejercicios/ejercicios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Ejercicio} from '../ejercicio';
+import {EJERCICIOS} from '../collection-ejercicios';
 
 @Component({
   selector: 'app-ejercicios',
@@ -8,16 +9,17 @@ import {Ejercicio} from '../ejercicio';
 })
 export class EjerciciosComponent implements OnInit {
 
-  ejercicio: Ejercicio={
-    id:1,
-    name:"Flexiones"
-  }
-
-
+  ejercicios=EJERCICIOS;
+  ejercicioSeleccionado:Ejercicio;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectEjercicio(ejercicio:Ejercicio):void{
+    console.log("Ejercicio seleccionado="+ejercicio.id);
+    this.ejercicioSeleccionado=ejercicio;
   }
 
 }
